afbc1149aa0ae5b80d02c5f155566eaaf2a523d9
verlangid <165430045+Verlangid11@users.noreply.github.com>
2024-12-23 13:42:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ mv /var/www/nebulafiles/nebula.sh /var/www/nebula.sh
 mv /var/www/nebulafiles/nebulaptero.zip /var/www/nebulaptero.zip
 rm -rf /var/www/nebulafiles
 echo "File nebula.sh berhasil dipindahkan ke /var/www/"
+cd /var/www/ 
+yes A | unzip nebulaptero.zip
 cd /var/www && bash nebula.sh
-cd /var/www/ && unzip nebulaptero.zip
